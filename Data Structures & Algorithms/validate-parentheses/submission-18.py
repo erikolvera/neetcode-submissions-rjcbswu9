ed7e2041bdfa5638ba9fb118537723b1df3155aa
Erik Olvera <118490997+erikolvera@users.noreply.github.com>
2026-05-31 19:17:46 -0500

    Add: validate-parentheses - submission-18

---
+++ Data Structures & Algorithms/validate-parentheses/submission-18.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        dic = {"}": "{", "]" : "[", ")":"("}
+
+        for c in s:
+            if c not in dic:
+                stack.append(c)
+            elif not stack or stack[-1] != dic[c]:
+                return False
+            else:
+                stack.pop()
+        return not stack
+
